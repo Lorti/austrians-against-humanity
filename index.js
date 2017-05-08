5f0e98a6bc9954efa81a1e15b349d158df3d9e48
mwieser <manuel.wieser@karriere.at>
2017-05-08 11:55:41 +0200

    Fix linebreaks.

--- index.js
+++ index.js
@@ -8,5 +8,11 @@ const questions = require('./questions.json');
 const source = fs.readFileSync(__dirname + '/template.hbs', 'utf8');
 const template = handlebars.compile(source);
 
+handlebars.registerHelper('breaklines', function(text) {
+    text = handlebars.Utils.escapeExpression(text);
+    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+    return new handlebars.SafeString(text);
+});
+
 fs.writeFileSync('answers.html', template({ class: 'answer', cards: answers }));
 fs.writeFileSync('questions.html', template({ class: 'question', cards: questions }));
